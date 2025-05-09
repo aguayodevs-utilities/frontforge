1bfe6d8ac3ddd0dfc0053585926393e9799f4433
Miguel Angel Aguayo Rivas <miguel.aguayo@coppel.com>
2025-05-09 03:49:05 -0600

    fix: Use CustomLayout in frontend template

--- templates/frontend/src/index.tsx
+++ templates/frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';  
-import { Layout, appTheme } from '@aguayodevs-utilities/preact-shared';
+import { CustomLayout, appTheme } from '@aguayodevs-utilities/preact-shared';
 import { TestComponent } from './components/TestComponent';
 import { ThemeProvider } from '@mui/material/styles';
 import "@aguayodevs-utilities/preact-shared/dist/static/css/base.css"; // importa css: estilos, variables, fuentes, etc. base
@@ -10,9 +10,9 @@ const App = () => {
   return (
     
     <ThemeProvider theme={appTheme}>
-      <Layout>
+      <CustomLayout>
           <TestComponent />
-      </Layout>
+      </CustomLayout>
     </ThemeProvider>
   );
 };
